529c21deac0fe35e1edfe3dc7ec7ab6033bcb3f8
padmini v <padmini.v@perfios.com>
2021-10-05 22:12:47 +0530

    REFACTORED: Function to calculate work hours

--- employeewagebuilder.js
+++ employeewagebuilder.js
@@ -1,14 +1,30 @@
 console.log("******** Welcome To Employee Wage Computation Application *********");
 
-//UC1
-const IS_ABSENT = 0
-let employeeCheck = Math.floor(Math.random() * 10) % 2;
+//uc3
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 
-if (employeeCheck == IS_ABSENT) 
+function getWorkingHours(employeeCheck)
 {
-    console.log("Employee is ABSENT");
-} 
-else 
-{
-    console.log("Employee is PRESENT");
+    switch (employeeCheck) 
+    {
+        case IS_PART_TIME:
+            console.log("Employee works Part Time");
+            return PART_TIME_HOURS;
+            
+        case IS_FULL_TIME:
+            console.log("Employee works Full Time");
+            return FULL_TIME_HOURS;
+            
+        default:
+            return 0;
+    }
 }
+let employeeHours = 0;
+let employeeCheck = Math.floor(Math.random() * 10) % 3;
+employeeHours = getWorkingHours(employeeCheck);
+let employeeWage = employeeHours * WAGE_PER_HOUR;
+console.log("Employee Wage = " + employeeWage);
