7cd432c9262486d6a82d498c1793c8bc993ac199
padmini v <padmini.v@perfios.com>
2021-10-05 22:25:19 +0530

    REFACTORED: Ability to calculate wages for month

--- employeewagebuilder.js
+++ employeewagebuilder.js
@@ -1,30 +1,30 @@
-console.log("******** Welcome To Employee Wage Computation Application *********");
+console.log("***Welcome To Employee Wage Builder Application***");
 
-//uc3
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(employeeCheck)
 {
-    switch (employeeCheck) 
+    switch(employeeCheck)
     {
-        case IS_PART_TIME:
-            console.log("Employee works Part Time");
+        case IS_PART_TIME : 
             return PART_TIME_HOURS;
-            
-        case IS_FULL_TIME:
-            console.log("Employee works Full Time");
-            return FULL_TIME_HOURS;
-            
+        case IS_FULL_TIME :
+             return FULL_TIME_HOURS;
         default:
             return 0;
     }
 }
-let employeeHours = 0;
-let employeeCheck = Math.floor(Math.random() * 10) % 3;
-employeeHours = getWorkingHours(employeeCheck);
-let employeeWage = employeeHours * WAGE_PER_HOUR;
-console.log("Employee Wage = " + employeeWage);
+
+let totalEmployeeHours = 0;
+for(let day = 0; day<NUM_OF_WORKING_DAYS; day++)
+{
+    employeeCheck = Math.floor(Math.random()*10)%3;
+    totalEmployeeHours += getWorkingHours(employeeCheck);
+}
+let employeeWage = totalEmployeeHours * WAGE_PER_HOUR;
+console.log("TotalHours: "+ totalEmployeeHours+"  Employee Wage: "+ employeeWage);
