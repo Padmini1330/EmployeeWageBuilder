9119165c908f20427cba2ecc6656b2c489ba3df1
padmini v <padmini.v@perfios.com>
2021-10-05 21:40:51 +0530

    REFACTORED: Ability to check if employee is present or absent

--- employeewagebuilder.js
+++ employeewagebuilder.js
@@ -2,7 +2,6 @@ console.log("******** Welcome To Employee Wage Computation Application *********
 
 const IS_ABSENT = 0
 let employeeCheck = Math.floor(Math.random() * 10) % 2;
-
 if (employeeCheck == IS_ABSENT) 
     console.log("Employee is ABSENT");
 else 
