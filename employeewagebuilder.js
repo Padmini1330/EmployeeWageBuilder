56067a8029b189a35c972ef049749fc9a0686b69
padmini v <padmini.v@perfios.com>
2021-10-06 22:40:03 +0530

    REFACTORED: ability to store employee daily wages in an array

--- employeewagebuilder.js
+++ employeewagebuilder.js
@@ -12,17 +12,16 @@ const MAX_HOURS_IN_MONTH = 100;
 let employeeHours = 0;
 let totalEmployeeHours = 0;
 let totalWorkingDays = 0;
+let employeeDailyWageArray = new Array();
 
-function getWorkingHours(employeeCheck)
+function getWorkingHours(employeeCheck) 
 {
     switch (employeeCheck) 
     {
         case IS_PART_TIME:
-            console.log("Employee works Part Time");
             return PART_TIME_HOURS;
 
         case IS_FULL_TIME:
-            console.log("Employee works Full Time");
             return FULL_TIME_HOURS;
 
         default:
@@ -30,12 +29,18 @@ function getWorkingHours(employeeCheck)
     }
 }
 
-while(totalEmployeeHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+function calculateDailyWage(employeeHours) 
 {
-    totalWorkingDays++;
-    employeeCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmployeeHours += getWorkingHours(employeeCheck);
+    return employeeHours * WAGE_PER_HOUR;
 }
 
-let employeeWage = totalEmployeeHours * WAGE_PER_HOUR;
-console.log("\nTotal Working Days = " + totalWorkingDays + "\nTotal Working Hours = " + totalEmployeeHours + "\nEmployee Wage = " + employeeWage);
+while (totalEmployeeHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) 
+{
+    totalWorkingDays++;
+    let employeeCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmployeeHours += getWorkingHours(employeeCheck);
+    employeeDailyWageArray.push(calculateDailyWage(employeeHours));
+}
+
+let totalEmployeeWage = calculateDailyWage(totalEmployeeHours);
+console.log("\nTotal Working Days = " + totalWorkingDays + "\nTotal Working Hours = " + totalEmployeeHours + "\nTotal Employee Wage = " + totalEmployeeWage);
